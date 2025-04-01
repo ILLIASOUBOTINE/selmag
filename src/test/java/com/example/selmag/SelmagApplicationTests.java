2c8aec033b46707150b16d2efb91c333eaaddf4e
LLIASOUBOTINE <semenuk1991311@gmail.com>
2025-04-01 21:52:24 +0300

    first commit

---
+++ src/test/java/com/example/selmag/SelmagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelmagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
