b6ebeabc847c14912bfbf14e00a85295bf2b14be
LLIASOUBOTINE <semenuk1991311@gmail.com>
2025-04-08 20:31:14 +0300

    load new project

---
+++ manager-app/src/main/java/ag/selm/manager/entity/Product.java
@@ -0,0 +1,19 @@
+package ag.selm.manager.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer id;
+
+    private String title;
+
+    private String details;
+
+}
