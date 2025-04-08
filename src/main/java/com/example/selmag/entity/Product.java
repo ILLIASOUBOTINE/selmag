004565467e70b29b5d7bcbd5461b436e63323608
LLIASOUBOTINE <semenuk1991311@gmail.com>
2025-04-08 15:05:18 +0300

    part 1

---
+++ src/main/java/com/example/selmag/entity/Product.java
@@ -0,0 +1,19 @@
+package com.example.selmag.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer id;
+
+    private String title;
+
+    private String details;
+
+}
---
+++ src/main/java/com/example/selmag/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.selmag.service;
+
+import com.example.selmag.entity.Product;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<Product> findAllProducts();
+
+    Product createProduct(String title, String details);
+
+    Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+}
